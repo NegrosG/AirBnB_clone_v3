4be6f5f7cc8a954726a381ce77cf31bc11968f72
Martins Onifade <negrosmartins@gmail.com>
2024-05-02 02:32:56 -0700

    index done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,12 +5,30 @@ Create Flask app, app_views
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
-def apui_status():
+def api_status():
     """
 
     """
     resp = {'status': "OK"}
     return jsonify(resp)
+
+@app_views.route('/stats')
+def get_stats():
+    """
+    stats if all objects route
+     :return: json of all objects
+    """
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+
+    return jsonify(stats)
